6cf9cd1ed5d2ee3f2c74bdc2819a9b4864538e90
Henok <henokmicrometre@gmail.com>
2022-04-29 11:28:31 +0100

    sorting dark

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,16 @@
 import Image  from "next/image";
+import Nav from "./Navbar";
 export default function Layout({ children }) {
   return (
     <>
       <div>
       <div className="relative flex flex-col justify-center min-h-screen py-6 overflow-hidden bg-green-500 sm:py-12">
-          <Image src="/images/beams.jpg" layout="fill" alt="" 
-          className=
-          "" 
-          width="1308" />
+          <Image src="/images/darkbg.jpg" layout="fill" alt=""
+           />
         </div>
-          {children}
+        <Nav />
       </div>
+      {children}
     </>
   );
 }
