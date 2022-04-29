d632195ee7de678bffc20573552866bfe85f1e22
Henok <henokmicrometre@gmail.com>
2022-04-29 11:43:16 +0100

    sorting dark

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -34,19 +34,8 @@ const features = [
 ];
 export function Hero() {
   return (
-      <div className="py-4 bg-white">
+      <div className="py-12 bg-white">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="lg:text-center">
-            <h1 className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-indigo-600 sm:text-4xl">
-            Electrical specialists serving surrey and surrounding areas.
-
-            </h1>
-            <p className="max-w-2xl mt-4 text-xl text-gray-500 lg:mx-auto">
-              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
-              magnam voluptatum cupiditate veritatis in accusamus quisquam.
-            </p>
-          </div>
-
           <div className="mt-10">
             <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
               {features.map((feature) => (
@@ -55,11 +44,11 @@ export function Hero() {
                     <div className="absolute flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
                       <feature.icon className="w-6 h-6" aria-hidden="true" />
                     </div>
-                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                    <p className="ml-16 text-lg font-medium leading-6 text-white">
                       {feature.name}
                     </p>
                   </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
+                  <dd className="mt-2 ml-16 text-base text-white">
                     {feature.description}
                   </dd>
                 </div>
--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,11 @@
-import Image  from "next/image";
+import Image from "next/image";
 import Nav from "./Navbar";
 export default function Layout({ children }) {
   return (
     <>
       <div>
-      <div 
-      >
-          <Image src="/images/darkbg.jpg" layout="fill" alt=""
-           />
+        <div>
+          <Image src="/images/darkbg.jpg" layout="fill" alt="layout image" />
         </div>
         <Nav />
       </div>
