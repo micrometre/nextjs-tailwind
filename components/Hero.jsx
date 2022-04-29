b09ab42a7fbeedd0e8315057c67d2e1dfcbefe0c
Henok <henokmicrometre@gmail.com>
2022-04-29 13:08:46 +0100

    sorting hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,30 +1,15 @@
 import Layout from "../components/Layout";
-import {
-  AnnotationIcon,
-  GlobeAltIcon,
-  LightningBoltIcon,
-  ScaleIcon,
-} from "@heroicons/react/outline";
-
-const features = [
-  {
-    name: "Electrical specialists serving surrey and surrounding areas.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: GlobeAltIcon,
-  }
-];
 export function Hero() {
   return (
-      <div className="py-12 mt-10 bg-white">
-              {features.map((feature) => (
-                <div key={feature.name} className="relative">
-                    <p className="text-4xl font-extrabold leading-6 tracking-tight text-white font sm:text-6xl">
-                      {feature.name}
-                    </p>
-                </div>
-              ))}
+    <div className="relative overflow-hidden bg-transparnet">
+      <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 ">
+        <div className="relative mx-auto px-7 sm:px-6 sm:static">
+            <h1 className="text-4xl font-extrabold tracking-tight text-white font sm:text-6xl">
+              Electrical specialists serving surrey and surrounding areas.
+            </h1>
+        </div>
       </div>
+    </div>
   );
 }
 
--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,32 @@
+import Head from "next/head";
 import Image from "next/image";
 import Nav from "./Navbar";
+
+const name = "MicrometreUK";
+export const siteTitle = "Electrical specialists";
+
 export default function Layout({ children }) {
   return (
     <>
+
+<Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="Electrical specialists serving surrey and surrounding areas."
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta
+          name="electricalsurrey Ltd"
+          content={siteTitle}
+        />
+        <title>{siteTitle}</title>
+      </Head>
+
+
       <div>
         <div>
           <Image src="/images/darkbg.jpg" layout="fill" alt="layout image" />
