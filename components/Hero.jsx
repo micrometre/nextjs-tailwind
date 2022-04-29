53fabb4ffc016f5bab1f7925d8a9ae68459491cf
Henok <henokmicrometre@gmail.com>
2022-04-29 12:01:12 +0100

    sorting dark

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,54 +8,22 @@ import {
 
 const features = [
   {
-    name: "Competitive exchange rates",
+    name: "Electrical specialists serving surrey and surrounding areas.",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
     icon: GlobeAltIcon,
-  },
-  {
-    name: "No hidden fees",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: ScaleIcon,
-  },
-  {
-    name: "Transfers are instant",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: LightningBoltIcon,
-  },
-  {
-    name: "Mobile notifications",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: AnnotationIcon,
-  },
+  }
 ];
 export function Hero() {
   return (
-      <div className="py-12 bg-white">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="mt-10">
-            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+      <div className="py-12 mt-10 bg-white">
               {features.map((feature) => (
                 <div key={feature.name} className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
-                      <feature.icon className="w-6 h-6" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 text-lg font-medium leading-6 text-white">
+                    <p className="text-4xl font-extrabold leading-6 tracking-tight text-white font sm:text-6xl">
                       {feature.name}
                     </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-white">
-                    {feature.description}
-                  </dd>
                 </div>
               ))}
-            </dl>
-          </div>
-        </div>
       </div>
   );
 }
